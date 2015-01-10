b517343575b7ef6ade117c2920745e107cede76f
Tyler <tyler.rinker@gmail.com>
2015-01-09 22:12:12 -0500

    added testing to improve coverage

--- tests/testthat/test-library_template.R
+++ tests/testthat/test-library_template.R
@@ -13,7 +13,24 @@ test_that("library_template creates a library package with correct components",{
     expect_true(file.exists(file.path(regex_lib, "NEWS")))
     expect_true(file.exists(file.path(regex_lib, "travis.yml")))
     unlink(regex_lib, recursive = TRUE, force = FALSE)
+})
 
+test_that("library_template creates a library package with correct components when optional names are set",{
+    
+    regex_lib <- file.path(tempdir(), "DELETE_ME")
+    suppressMessages(library_template(regex_lib, 
+        email = "tyler.rinker@gmail.com",
+        github.user = "trinker",
+        name = c(first="Tyler", middle = "W.", last="Rinker")))    
+    expect_true(file.exists(regex_lib))
+    expect_true(file.exists(file.path(regex_lib, "README.md")))
+    expect_true(file.exists(file.path(regex_lib, "tests")))
+    expect_true(file.exists(file.path(regex_lib, "tests/testthat")))
+    expect_true(file.exists(file.path(regex_lib, "tests/testthat.R")))
+    expect_true(file.exists(file.path(regex_lib, "DESCRIPTION")))
+    expect_true(file.exists(file.path(regex_lib, "NEWS")))
+    expect_true(file.exists(file.path(regex_lib, "travis.yml")))
+    unlink(regex_lib, recursive = TRUE, force = FALSE)
 })
 
 
