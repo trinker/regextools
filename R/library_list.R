5251ef35d2877eeae66bbc8ae541758b132abd91
Tyler <tyler.rinker@gmail.com>
2015-01-15 23:05:17 -0500

    added ability to name library_list

--- R/library_list.R
+++ R/library_list.R
@@ -5,6 +5,8 @@
 #' @param path The path to the regular expression library package.
 #' @param out The file path to output the .rda file (see 
 #' \code{\link[base]{save}} for details on saving).
+#' @param name The name to assign the list to and to use for the .rda file.  If
+#' \code{NULL} the package name is used.
 #' @param \ldots Other arguments passed to internal functions.
 #' @return Generates a minimal .rda regular expression library.  To load the 
 #' .rda file use \code{\link[base]{load}}.
@@ -21,7 +23,7 @@
 #' sample             ## sample is now overwritten with the sample regex list
 #' rm(sample)         ## remove the list (restore default sample)
 #' }
-library_list <- function(path, out = NULL, ...){
+library_list <- function(path, out = NULL, name = NULL, ...){
 
     ## Document and Install package path
     build_install(path = path, ...)
@@ -29,12 +31,14 @@ library_list <- function(path, out = NULL, ...){
     ## grab the regex meta info
     regsmeta <- grab_regexpr_meta(path)
 
+    if (is.null(name)) name <- regsmeta[["Package"]]
+    
     ## grab the regexes
     env <- new.env(hash = FALSE)
-    assign(regsmeta[["Package"]], grab_regexpr(path), envir=env)
+    assign(name, grab_regexpr(path), envir=env)
 
-    if (is.null(out)) out <- sprintf("%s.rda", regsmeta[["Package"]])
-    save(list = regsmeta[["Package"]], file=out, envir = env)
+    if (is.null(out)) out <- sprintf("%s.rda", name)
+    save(list = name, file=out, envir = env)
 
-    if (file.exists(out)) message(sprintf("%s list generated", regsmeta[["Package"]]))
+    if (file.exists(out)) message(sprintf("%s list generated", name))
 }
